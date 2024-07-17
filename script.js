4e0fc8768f8328ab0aa31aa167924e700021a90d
unknown <joaoevangelista12>
2024-07-17 13:49:06 -0300

    dom

---
+++ script.js
@@ -0,0 +1,16 @@
+// selecione o botão usando o método getElementById
+var botao = document.getElementById("botao");
+var limpar = document.getElementById("limpar");
+
+// adicione um evento de clique ao botão
+botao.addEventListener("click", function() {
+    // selecione o parágrafo usando o método getElementById
+    var paragrafo = document.getElementById("paragrafo");
+    // altere o texto do parágrafo
+    paragrafo.textContent = "O texto deste parágrafo foi alterado!";
+});
+
+limpar.addEventListener("click", function() {
+    var paragrafo = document.getElementById("paragrafo");
+    paragrafo.textContent = "";
+});
